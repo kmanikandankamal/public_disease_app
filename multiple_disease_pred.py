ba2d61274e278eb692b7bbbeeeac7e5f19884313
kmanikandankamal <126862560+kmanikandankamal@users.noreply.github.com>
2023-03-03 15:07:43 +0530

    Update multiple_disease_pred.py

--- multiple_disease_pred.py
+++ multiple_disease_pred.py
@@ -5,7 +5,7 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('/home/manikandan/Downloads/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open("C:/home/manikandan/Downloads/diabetes_model.sav", 'rb'))
 
 heart_disease_model = pickle.load(open('/home/manikandan/Downloads/heart_disease_model.sav','rb'))
 
